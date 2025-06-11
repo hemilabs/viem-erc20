59eca5bd1627365e5384b98a20ba331abe8c4c84
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-06-11 16:11:51 -0300

    Add 'name' method

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 export { getErc20TokenAllowance } from "../public/allowance.js";
 export { getErc20TokenBalance } from "../public/balance.js";
 export { getErc20TokenDecimals } from "../public/decimals.js";
+export { getErc20TokenName } from "../public/name.js";
 export { getErc20TokenTotalSupply } from "../public/totalSupply.js";
 export { approveErc20Token } from "../wallet/approve.js";
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { type Account, type Chain, type Client, type Transport } from "viem";
 import { getErc20TokenAllowance } from "./public/allowance.js";
 import { getErc20TokenBalance } from "./public/balance.js";
 import { getErc20TokenDecimals } from "./public/decimals.js";
+import { getErc20TokenName } from "./public/name.js";
 import { getErc20TokenTotalSupply } from "./public/totalSupply.js";
 import { approveErc20Token } from "./wallet/approve.js";
 
@@ -24,6 +25,8 @@ export const erc20PublicActions =
     getErc20TokenDecimals: (
       params: Parameters<typeof getErc20TokenDecimals>[1],
     ) => getErc20TokenDecimals(client, params),
+    getErc20TokenName: (params: Parameters<typeof getErc20TokenName>[1]) =>
+      getErc20TokenName(client, params),
     getErc20TokenTotalSupply: (
       params: Parameters<typeof getErc20TokenTotalSupply>[1],
     ) => getErc20TokenTotalSupply(client, params),
